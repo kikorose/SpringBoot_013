5510a7e2e07d578985ce7b0f9db37fbc93f8d276
kikorose <nakelia78@gmil.com>
2017-08-23 13:29:30 -0400

    first commit(database relationships many to many)

---
+++ src/main/java/com/example/demo/DemoSpringBoot013Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBoot013Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBoot013Application.class, args);
+	}
+}
